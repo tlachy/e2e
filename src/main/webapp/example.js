5b1589eb1e08cf80ce69d6dade168c7e872dab81
tlachy <tlachy@gmail.com>
2014-06-10 14:37:13 +0200

    - interview line added

--- src/main/webapp/example.js
+++ src/main/webapp/example.js
@@ -2,6 +2,8 @@ angular.module('plunker', ['ui.bootstrap']);
 
 var TabsDemoCtrl = function ($scope, $modal, $log) {
 
+  drawShape();
+
   $scope.tabs = [
     { title:"Dynamic Title 1", content:"Dynamic content 1" },
     { title:"Dynamic Title 2", content:"Dynamic content 2", disabled: true }
@@ -85,6 +87,54 @@ var TabsDemoCtrl = function ($scope, $modal, $log) {
 
   $scope.items = ['item1', 'item2', 'item3'];
 };
+
+
+function drawShape(){
+
+  var canvas = document.getElementById('mycanvas');
+  canvas.width = screen.width;
+
+  // Make sure we don't execute when canvas isn't supported
+  if (canvas.getContext){
+
+    //number of interview procedure steps
+    var steps = 5;
+    // use getContext to use the canvas for drawing
+    var ctx = canvas.getContext('2d');
+
+    // Filled triangle
+    ctx.beginPath();
+    ctx.lineWidth = 4;
+    ctx.strokeStyle = '#333';
+    ctx.moveTo(30,30);
+    ctx.lineTo(screen.width - 30,30);
+    console.log(screen.width);
+    ctx.closePath();
+    ctx.stroke();
+
+
+    ctx.beginPath();
+    ctx.lineWidth = 4;
+    ctx.strokeStyle = '#333';
+
+    var oneStepWidth = (screen.width - 30)/steps;
+
+    for(var i = 1; i < steps; i++){
+        ctx.moveTo(30 + i*oneStepWidth,30);
+        ctx.lineTo(30 + i*oneStepWidth,40);
+        ctx.lineTo(30 + i*oneStepWidth,20);
+    }
+
+    ctx.closePath();
+    ctx.stroke();
+
+
+
+  } else {
+    alert('You need Safari or Firefox 1.5+ to see this demo.');
+  }
+}
+
 
 // Please note that $modalInstance represents a modal window (instance) dependency.
 // It is not the same as the $modal service used above.
---
+++ src/main/webapp/interview.js
@@ -0,0 +1,52 @@
+angular.module('plunker', ['ui.bootstrap']);
+
+var TabsDemoCtrl = function ($scope, $modal, $log) {
+  drawShape();
+};
+
+
+function drawShape(){
+
+  var canvas = document.getElementById('mycanvas');
+  canvas.width = screen.width;
+
+  // Make sure we don't execute when canvas isn't supported
+  if (canvas.getContext){
+
+    //number of interview procedure steps
+    var steps = 5;
+    // use getContext to use the canvas for drawing
+    var ctx = canvas.getContext('2d');
+
+    // Filled triangle
+    ctx.beginPath();
+    ctx.lineWidth = 4;
+    ctx.strokeStyle = '#333';
+    ctx.moveTo(30,30);
+    ctx.lineTo(screen.width - 30,30);
+    console.log(screen.width);
+    ctx.closePath();
+    ctx.stroke();
+
+
+    ctx.beginPath();
+    ctx.lineWidth = 4;
+    ctx.strokeStyle = '#333';
+
+    var oneStepWidth = (screen.width - 30)/steps;
+
+    for(var i = 1; i < steps; i++){
+        ctx.moveTo(30 + i*oneStepWidth,30);
+        ctx.lineTo(30 + i*oneStepWidth,40);
+        ctx.lineTo(30 + i*oneStepWidth,20);
+    }
+
+    ctx.closePath();
+    ctx.stroke();
+
+
+
+  } else {
+    alert('You need Safari or Firefox 1.5+ to see this demo.');
+  }
+}
