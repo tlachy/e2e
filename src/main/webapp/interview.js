a9db504d45ea6803fd72a857a60a0be741dcf292
tlachy <tlachy@gmail.com>
2014-06-13 10:51:04 +0200

    - interview widget first draft - next to solve how to position applicants

--- src/main/webapp/interview.js
+++ src/main/webapp/interview.js
@@ -1,51 +1,23 @@
 angular.module('plunker', ['ui.bootstrap']);
 
-var TabsDemoCtrl = function ($scope, $modal, $log) {
-    drawInterviewLine(5);
+var interviewControler = function ($scope, $modal, $log) {
+    var interview = {interviewId:5,
+                     interviewName:'Java Developer',
+                     interviewDesc:'We are looking for senior java dev with spring and wicket',
+                     phases:['application','CV review', 'Java test', 'Skype interview', 'Face-to-Face interview', 'Job offer', 'Offer accepted']};
+
+
+    //drawInterviewLine(interview.phases.length);
+
+    var applicantOne =   {interviewId:5,personId:1,interviewPhase:3};
+    var applicantTwo =   {interviewId:5,personId:2,interviewPhase:2};
+    var applicantThree = {interviewId:5,personId:3,interviewPhase:2};
+
+    var applicants = [];
+    applicants.push(applicantOne);
+    applicants.push(applicantTwo);
+    applicants.push(applicantThree);
+
+    $scope.applicantGroups = ["neco","necodelsi","nejdelsizevseho"];
+    $scope.phases = interview.phases;
 };
-
-
-
-function drawInterviewLine(numberOfSteps){
-
-    var canvas = document.getElementById('mycanvas');
-    canvas.width = screen.width;
-
-    // Make sure we don't execute when canvas isn't supported
-    if (canvas.getContext){
-
-            //number of interview procedure steps
-            var steps = numberOfSteps;
-            // use getContext to use the canvas for drawing
-            var ctx = canvas.getContext('2d');
-
-            // Filled triangle
-            ctx.beginPath();
-            ctx.lineWidth = 4;
-            ctx.strokeStyle = '#333';
-            ctx.moveTo(30,30);
-            ctx.lineTo(screen.width - 30,30);
-            console.log(screen.width);
-            ctx.closePath();
-            ctx.stroke();
-
-
-            ctx.beginPath();
-            ctx.lineWidth = 4;
-            ctx.strokeStyle = '#333';
-
-            var oneStepWidth = (screen.width - 30)/steps;
-
-            for(var i = 1; i < steps; i++){
-                ctx.moveTo(30 + i*oneStepWidth,30);
-                ctx.lineTo(30 + i*oneStepWidth,40);
-                ctx.lineTo(30 + i*oneStepWidth,20);
-            }
-
-            ctx.closePath();
-            ctx.stroke();
-
-    } else {
-    alert('You need Safari or Firefox 1.5+ to see this demo.');
-    }
-}
