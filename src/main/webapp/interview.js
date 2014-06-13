45df4ecbbcb9bc12e5b9105a7ada7ba2ad3d1c21
tlachy <tlachy@gmail.com>
2014-06-13 13:44:45 +0200

    - interview widget first draft - grouping implemented - next to solve how to position applicants

--- src/main/webapp/interview.js
+++ src/main/webapp/interview.js
@@ -4,20 +4,67 @@ var interviewControler = function ($scope, $modal, $log) {
     var interview = {interviewId:5,
                      interviewName:'Java Developer',
                      interviewDesc:'We are looking for senior java dev with spring and wicket',
-                     phases:['application','CV review', 'Java test', 'Skype interview', 'Face-to-Face interview', 'Job offer', 'Offer accepted']};
+                     phases:['application','CV review', 'Java test', 'Skype interview', 'Face-to-Face interview', 'Job offer', 'Offer accepted'],
+                     startDate: "neco",
+                     endDate: "neco",
+                     status: "closed",
+                     createdBy: "personID or The person object?"};
 
-
-    //drawInterviewLine(interview.phases.length);
-
-    var applicantOne =   {interviewId:5,personId:1,interviewPhase:3};
-    var applicantTwo =   {interviewId:5,personId:2,interviewPhase:2};
-    var applicantThree = {interviewId:5,personId:3,interviewPhase:2};
+    var a1 = {interviewId:5,personId:1,interviewPhase:6};
+    var a2 = {interviewId:5,personId:2,interviewPhase:6};
+    var a3 = {interviewId:5,personId:3,interviewPhase:6};
+    var a4 = {interviewId:5,personId:3,interviewPhase:6};
+    var a5 = {interviewId:5,personId:3,interviewPhase:5};
+    var a6 = {interviewId:5,personId:3,interviewPhase:4};
+    var a7 = {interviewId:5,personId:3,interviewPhase:3};
+    var a8 = {interviewId:5,personId:3,interviewPhase:2};
+    var a9 = {interviewId:5,personId:3,interviewPhase:1};
 
     var applicants = [];
-    applicants.push(applicantOne);
-    applicants.push(applicantTwo);
-    applicants.push(applicantThree);
+    applicants.push(a1);
+    applicants.push(a2);
+    applicants.push(a3);
+    applicants.push(a4);
+    applicants.push(a5);
+    applicants.push(a6);
+    applicants.push(a7);
+    applicants.push(a8);
 
-    $scope.applicantGroups = ["neco","necodelsi","nejdelsizevseho"];
+    $scope.applicantGroups = createApplicantGroups(applicants, 3);
     $scope.phases = interview.phases;
+
+    $scope.showApplicantsGroup = function(group){
+        console.log(group);
+    }
+
 };
+
+var createApplicantGroups = function(applicants, limitOfApplicantsInOnePhase){
+
+    var groupedByInterviewPhase = {};
+
+    angular.forEach(applicants, function(applicant) {
+
+           if( Object.keys(groupedByInterviewPhase).indexOf(applicant.interviewPhase.toString()) === -1 ){
+                groupedByInterviewPhase[applicant.interviewPhase] = [applicant];
+           } else{
+                groupedByInterviewPhase[applicant.interviewPhase].push(applicant);
+           }
+         });
+
+
+    var applicantGroups = [];
+
+    angular.forEach(groupedByInterviewPhase, function(group, interviewPhase) {
+
+        if( group.length <= limitOfApplicantsInOnePhase ){
+            angular.forEach(group, function(applicant){   applicantGroups.push([applicant]);   });
+        } else{
+            applicantGroups.push(group);
+        }
+    });
+
+
+
+    return applicantGroups;
+}
