ca4378286fb576b58783b81616aeb0f4a5d7990a
tlachy <tlachy@gmail.com>
2014-09-02 22:29:31 +0200

    dropwizzard handles static resources not form webapp but from resources/assets - so I moved it - it's a bit strange

--- src/main/java/com/gmail/tlachy/e2e/conf/InterviewApplication.java
+++ src/main/java/com/gmail/tlachy/e2e/conf/InterviewApplication.java
@@ -20,6 +20,8 @@ public class InterviewApplication extends Application<InterviewConfiguration>  {
 		// I would like to write it the way that later one server can take care of just one resource
 		//////////////////
 
+		environment.jersey().setUrlPattern("/service/*");
+
 		final CompanyResource companyResource = new CompanyResource(configuration.getTemplate());
 		final PersonResource personResource = new PersonResource(configuration.getTemplate());
 
@@ -35,12 +37,7 @@ public class InterviewApplication extends Application<InterviewConfiguration>  {
 
 	@Override
 	public void initialize(Bootstrap<InterviewConfiguration> bootstrap) {
-
-		//This tells Dropwizard that we want to take resources stored under /src/main/resources/assets and serve them
-		// from the root of our application (/) and that we want index.html to be the default index file name.
-		bootstrap.addBundle(new AssetsBundle("/assets/", "/", "index.html"));
+		bootstrap.addBundle(new AssetsBundle("/assets/", "/","html/common/index.html","html"));
 	}
-
-
 
 }
